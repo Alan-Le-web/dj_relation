b5a4fbd32f88dda4d0be8d8022f56dbebc850968
Alan Le <aal.py@yandex.ru>
2025-04-06 13:56:02 +0400

    правки

--- articles/admin.py
+++ articles/admin.py
@@ -1,8 +1,25 @@
 from django.contrib import admin
+from django.core.exceptions import ValidationError
+from django.forms import BaseInlineFormSet
+
 from .models import Scope, Article, Tag
+
+class ScopeInlineFormset(BaseInlineFormSet):
+    def clean(self):
+        super().clean()
+        tag_count = 0
+        for form in self.forms:
+            if form.cleaned_data.get('is_main'):
+               tag_count +=1
+        if tag_count !=1:
+            raise ValidationError('Должен быть только один основной тег!')
+
+
+
 
 class ScopeInline(admin.TabularInline):
     model = Scope
+    formset = ScopeInlineFormset
     extra = 1
 
 @admin.register(Tag)
